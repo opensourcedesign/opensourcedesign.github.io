e2e9f6a2b23b2ee35aaf5b5608c8393b99b976d5
mrwils@tutanota.com <mrwils@tutanota.com>
2019-02-07 12:00:18 +0100

    Add license information to JS files

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache 2.0
 function foo(response) {
   var meta = response.meta;
   var data = response.data;
@@ -193,3 +194,4 @@ $(document).ready(function() {
     }
   });
 });
+// @license-end
